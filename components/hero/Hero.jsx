297087a344e7fdbcbc81104ef80211e2511bbbd7
utkarshk806 <92133638+utkarshk806@users.noreply.github.com>
2023-08-07 08:58:30 +0530

    updated

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -1,17 +1,20 @@
-import Image from 'next/image'
-import styles from './Hero.module.css'
+import Image from "next/image";
+import styles from "./Hero.module.css";
 
-export default function Hero(){
-
-
-return <div className={styles.container}>
-
-    <div className={styles.user}>
-        <Image style={{borderRadius:'50%'}} src={'/images/site/max.png'} alt="Max" width="200" height="200"/>
+export default function Hero() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.user}>
+        <Image
+          style={{ borderRadius: "50%" }}
+          src={"/images/site/max.png"}
+          alt="Max"
+          width="200"
+          height="200"
+        />
         <h1 className={styles.name}>Utkarsh Kumar</h1>
-        <p className={styles.description}>I blog about web developent</p>
+        <p className={styles.description}>I write about web developent</p>
+      </div>
     </div>
-</div>
-
-
+  );
 }
